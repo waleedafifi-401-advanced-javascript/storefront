614bf456e6e30f65f5c58f09d9fbc104b753cee9
Waleed A. Afifi <waleedafifich@gmail.com>
2020-10-26 23:50:14 +0200

    Finish phase 2

--- src/components/header.js
+++ src/components/header.js
@@ -1,16 +1,41 @@
 import React from 'react';
 import { Grid, AppBar, CssBaseline, Toolbar, Typography  } from '@material-ui/core';
+import { connect } from 'react-redux';
 
-export default () => (
-    <>
-        <CssBaseline />
-        <AppBar position="static" elevation={0} className="MuiAppBar-root">
-            <Toolbar className="">
-                <Grid container justify="space-between" alignItems="center" >
+const Header = (props) => {
+    return (
+        <React.Fragment>
+            <CssBaseline />
+            <AppBar position="static" elevation={0} className="MuiAppBar-root">
+                <Toolbar className="">
+                    <Grid container justify="space-between" alignItems="center" >
                     <Typography variant="h4">Storefront</Typography>
-                    <Typography variant="h6">Cart (1)</Typography>
+        
+                    <Typography variant="h6">Cart ({props.cart.length})</Typography>
                 </Grid>
-            </Toolbar>
-        </AppBar>
-    </>
-)
+                </Toolbar>
+            </AppBar>
+        </React.Fragment>
+    );
+}
+    
+const mapStateToProps = store => {
+    return {
+        cart: store.cart.items,
+    }
+}
+    
+export default connect(mapStateToProps)(Header)
+// export default () => (
+//     <React.Fragment>
+//         <CssBaseline />
+//         <AppBar position="static" elevation={0} className="MuiAppBar-root">
+//             <Toolbar className="">
+//                 <Grid container justify="space-between" alignItems="center" >
+//                     <Typography variant="h4">Storefront</Typography>
+//                     <Typography variant="h6">Cart (1)</Typography>
+//                 </Grid>
+//             </Toolbar>
+//         </AppBar>
+//     </React.Fragment>
+// )
--- src/store/index.js
+++ src/store/index.js
@@ -4,9 +4,11 @@ import { composeWithDevTools } from 'redux-devtools-extension';
 
 import category from './categories';
 import product from './products';
+import cart from './cart';
+
 console.log(category)
 
-let reducers = combineReducers({ category , product });
+let reducers = combineReducers({ category , product , cart});
 
 const store = () => {
   return createStore(reducers, composeWithDevTools());
